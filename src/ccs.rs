fbdf72f972a1ee72efd358c290dcdffa763f3d07
George Kadianakis <desnacked@riseup.net>
2023-06-06 00:08:29 +0300

    Add test demonstrating the compressing behavior of the v_i in LCCCS

--- src/ccs.rs
+++ src/ccs.rs
@@ -525,4 +525,55 @@ pub mod test {
             ccs.compute_c_from_sigmas_and_thetas(&sigmas, &thetas, gamma, &beta, &r_x, &r_x_prime);
         assert_eq!(c, expected_c);
     }
+
+
+    /// Given M(x,y) matrix and a random field element `r`, test that ~M(r,y) is is an s'-variable polynomial which
+    /// compresses every column j of the M(x,y) matrix by performing a random linear combination between the elements
+    /// of the column and the values eq_i(r) where i is the row of that element
+    ///
+    /// For example, for matrix M:
+    ///
+    /// [2, 3, 4, 4
+    ///  4, 4, 3, 2
+    ///  2, 8, 9, 2
+    ///  9, 4, 2, 0]
+    ///
+    /// The polynomial ~M(r,y) is a polynomial in F^2 which evaluates to the following values in the hypercube:
+    /// - M(00) = 2*eq_00(r) + 4*eq_10(r) + 2*eq_01(r) + 9*eq_11(r)
+    /// - M(10) = 3*eq_00(r) + 4*eq_10(r) + 8*eq_01(r) + 4*eq_11(r)
+    /// - M(01) = 4*eq_00(r) + 3*eq_10(r) + 9*eq_01(r) + 2*eq_11(r)
+    /// - M(11) = 4*eq_00(r) + 2*eq_10(r) + 2*eq_01(r) + 0*eq_11(r)
+    ///
+    /// This is used by Hypernova in LCCCS to perform a verifier-chosen random linear combination between the columns
+    /// of the matrix and the z vector. This technique is also used extensively in "An Algebraic Framework for
+    /// Universal and Updatable SNARKs".
+    #[test]
+    fn test_compute_M_r_y_compression() -> () {
+        let mut rng = test_rng();
+
+        // s = 2, s' = 3
+        let ccs = get_test_ccs();
+
+        let M = ccs.M[0].clone();
+        let M_mle = matrix_to_mle(M.clone());
+
+        // Fix the polynomial ~M(r,y)
+        let r: Vec<Fr> = (0..ccs.s).map(|_| Fr::rand(&mut rng)).collect();
+        let M_r_y = fix_last_variables(&M_mle, &r);
+
+        // Now let's compute M_r_y the other way around
+        for j in 0..M[0].len() {
+            // Go over every column of M
+            let column_j: Vec<Fr> = M.clone().iter().map(|x| x[j]).collect();
+
+            // and perform the random lincomb between the elements of the column and eq_i(r)
+            let rlc = BooleanHypercube::new(ccs.s)
+                .enumerate()
+                .into_iter()
+                .map(|(i, x)| column_j[i] * eq_eval(&x, &r).unwrap())
+                .fold(Fr::zero(), |acc, result| acc + result);
+
+            assert_eq!(M_r_y.evaluations[j], rlc);
+        }
+    }
 }
